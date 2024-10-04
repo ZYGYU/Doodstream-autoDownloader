8f0f14798c6409b69451884fe86b057e80678223
ZYGYU <163495115+ZYGYU@users.noreply.github.com>
2024-10-04 12:31:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ async def scrape(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8100)
